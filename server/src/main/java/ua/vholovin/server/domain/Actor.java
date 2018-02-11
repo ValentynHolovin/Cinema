925192134d6ac6752fa22e66b3b768d63443ed48
ValentynHolovin <valentyn.holovin@gmail.com>
2018-02-11 04:39:49 +0200

    before sleep

--- server/src/main/java/ua/vholovin/server/domain/Actor.java
+++ server/src/main/java/ua/vholovin/server/domain/Actor.java
@@ -4,14 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.sql.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Actor {
+@Entity
+@Table(name = "actor")
+public class Actor implements Serializable {
+    @Id()
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ActorID", unique = true, nullable = false, updatable = false, insertable = false)
     private Integer actorID;
+    @Column(name = "FirstName", nullable = false, length = 45)
     private String firstName;
+    @Column(name = "LastName", nullable = false, length = 45)
     private String lastName;
+    @Column(name = "ModifiedDate")
+    @Temporal(TemporalType.TIMESTAMP)
     private Date modifiedDate;
 }
