98410a3219035c287ada2e1bc4e99f8082e3db47
ValentynHolovin <valentyn.holovin@gmail.com>
2018-02-11 20:41:51 +0200

    before sleep

--- server/src/main/java/ua/vholovin/server/domain/Actor.java
+++ server/src/main/java/ua/vholovin/server/domain/Actor.java
@@ -3,26 +3,38 @@ package ua.vholovin.server.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.*;
 
 import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.io.Serializable;
-import java.util.Date;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "actor")
+@Table(name = "actor", uniqueConstraints = {@UniqueConstraint(columnNames = {"FirstName", "LastName"})})
 public class Actor implements Serializable {
     @Id()
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "ActorID", unique = true, nullable = false, updatable = false, insertable = false)
+    @Column(name = "ActorID")
     private Integer actorID;
+
     @Column(name = "FirstName", nullable = false, length = 45)
     private String firstName;
+
     @Column(name = "LastName", nullable = false, length = 45)
     private String lastName;
+
+    @Column(name = "CreatedDate")
+    //@Generated(value = GenerationTime.INSERT)
+    //@CreationTimestamp
+    private LocalDateTime createdDate;
+
     @Column(name = "ModifiedDate")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date modifiedDate;
+    @Generated(value = GenerationTime.ALWAYS)
+    private Timestamp modifiedDate;
 }
